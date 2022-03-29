e38f31610f484432fb2c8e5bfe8b61dee8ca95ed
ilyaz <so.ilia@yandex.ru>
2022-03-29 04:08:20 +0300

    Добавил валидацию вьюху и контроллер для новой модели

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_29_000336) do
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
