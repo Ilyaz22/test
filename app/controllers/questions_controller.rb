723abd12061bf007949f28cbcf9ecab94b10b812
ilyaz <so.ilia@yandex.ru>
2022-04-02 03:50:54 +0300

    начал работу над edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,36 @@
 class QuestionsController < ApplicationController
 
+  def edit
+    @question = Question.find_by id: params[:id]
+    
+  end
+
   def index
     @questions = Question.all
   end
 
 
   def new
-    @question = Question.new
+    @question = Question.new #показывает форму,данные отпраляются в хеш парамс
   end
 
 
   def create
-    render plain: params
+    #render plain: params
+    @question = Question.new(question_params)
+    if @question.save # если ошибок нет
+      redirect_to questions_path
+    else
+      render :new # опять рендерить форму new если есть ошибка (валидация)
+    end
   end
 
+
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body) # даем разр на заполн опр полей
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  #resources :questions
-  get '/questions' , to: 'questions#index'
-  get '/questions/new' , to: 'questions#new'
-  post '/questions' , to: 'questions#create'
+  resources :questions 
+  # get '/questions' , to: 'questions#index'
+
+  # get '/questions/new' , to: 'questions#new'
+
+  # get '/questions/:id/edit' , to: 'questions#edit'
+
+  # post '/questions' , to: 'questions#create'
 
 end
