8adc6426c7ea38c20aef394db98fc5a0c7fe989f
ilyaz <so.ilia@yandex.ru>
2022-04-05 04:34:28 +0300

    добавил обработку ошибок отс id через главн контроллер и concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  
+  include ErrorHandling
+
+
+  # rescue_from ActiveRecord::RecordNotFound, with: :notfound #обрабат ошибку не найден методом notfound
+
+  # private
+
+  # def notfound(exception) #метод принимает ошибку
+  #   logger.warn exception # записывает в журнал рельс эту ошибку
+  #   render file: 'public/404.html' , status: :not_found, layout: false # не использует лейаут
+
+  # end
+
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,23 +1,24 @@
 class QuestionsController < ApplicationController
 
+  before_action :set_question, only: %i[show destroy edit update] #запускается первым
+
   def show
-    @question = Question.find_by id: params[:id]
+
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
+
     @question.destroy
     flash[:success] = "Question deleted"
     redirect_to questions_path
   end
 
   def edit
-    @question = Question.find_by id: params[:id] #1-ый id: это поле по которому поиск
+  
   end
 
 
   def update
-    @question = Question.find_by id: params[:id] #1-ый id: это поле по которому поиск
     if @question.update(question_params) # если ошибок нет, обновить в базе
       flash[:success] = "Question updated!"
       redirect_to questions_path
@@ -53,6 +54,13 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body) # даем разр на заполн опр полей
+  end
+
+  def set_question
+
+  #@question = Question.find_by id: params[:id]#1-ый id: это поле по которому поиск
+  @question = Question.find params[:id] # если указан несущ id, то в главн контр обр ошибка
+
   end
 
 end
